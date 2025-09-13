8db8ea32edae6cc4213c12b2b3ca52d32c81a612
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2025-09-13 21:46:11 +0200

    Updated order page controller
    Added the `edit_orders_view` and the parameters `user_id` and
    `has_edit_permission` to be able to set the `order.updated_by` column
    when a user updates an order, given he has permission to do so.

--- src/cambiato/app/controller/order.py
+++ src/cambiato/app/controller/order.py
@@ -7,27 +7,37 @@ from zoneinfo import ZoneInfo
 import streamlit as st
 
 # Local
-from cambiato.app.components.buttons import create_order_button
-from cambiato.app.components.selectors import utility_pills_selector
+from cambiato.app.components import ICON_INFO, create_order_button, utility_pills_selector
 from cambiato.app.database import (
     get_all_checklists_cached,
     get_all_facilities_cached,
     get_all_order_statuses_cached,
     get_all_order_types_cached,
+    get_all_orders_cached,
     get_all_technicians_cached,
     get_all_utilities_cached,
 )
+from cambiato.app.views import edit_orders_view
 from cambiato.database import Session
 from cambiato.models.dataframe import ChecklistDataFrameModel, FacilityDataFrameModel
-from cambiato.translations import Database, Order, create_translation_mapping
+from cambiato.translations import (
+    Database,
+    EditOrdersDataFrame,
+    EditOrdersView,
+    Order,
+    create_translation_mapping,
+)
 
 
 def controller(
     session: Session,
     order_translation: Order,
     db_translation: Database,
+    edit_orders_df_trans: EditOrdersDataFrame,
+    edit_orders_view_trans: EditOrdersView,
     tz: ZoneInfo,
-    user_id: str | None,
+    user_id: str,
+    has_edit_permission: bool = True,
 ) -> None:
     r"""Render the order page.
 
@@ -42,11 +52,18 @@ def controller(
     db_translation: cambiato.translations.Database
         Translations for default database objects.
 
+    edit_orders_df_trans : cambiato.translations.EditOrdersDataFrame
+        The translations for the order DataFrame editor component
+        :func:`cambiato.app.components.edit_orders`.
+
     tz : zoneinfo.ZoneInfo
         The timezone where the application is used.
 
-    user : str or None
-        The ID of the signed in user. None if the user is not signed in.
+    user_id : str
+        The ID of the signed in user.
+
+    has_edit_permission : bool, default False
+        True if the user has permission to edit orders and False otherwise.
     """
 
     st.title(order_translation.page_title)
@@ -55,9 +72,11 @@ def controller(
         _session=session, translation=create_translation_mapping(db_translation.utility)
     )
 
-    selected_utility = utility_pills_selector(
-        label='Utility', utilities=utilities, default=0, label_visibility='collapsed'
-    )
+    left_col, right_col, _ = st.columns((3, 1, 6))
+    with left_col:
+        selected_utility = utility_pills_selector(
+            label='Utility', utilities=utilities, default=0, label_visibility='collapsed'
+        )
 
     technicians = get_all_technicians_cached(_session=session)
 
@@ -72,28 +91,55 @@ def controller(
         checklists = ChecklistDataFrameModel()
         create_order_button_disabled = True
 
+    order_type_trans = create_translation_mapping(db_translation.order_type)
     order_types = get_all_order_types_cached(
-        _session=session,
-        utility_ids=utility_ids,
-        translation=create_translation_mapping(db_translation.order_type),
-    )
-    order_statuses = get_all_order_statuses_cached(
-        _session=session,
-        utility_ids=utility_ids,
-        translation=create_translation_mapping(db_translation.order_status),
+        _session=session, utility_ids=utility_ids, translation=order_type_trans
     )
 
-    create_order_button(
-        label=order_translation.create_order_button.label,
-        disabled=create_order_button_disabled,
+    order_status_trans = create_translation_mapping(db_translation.order_status)
+    order_statuses = get_all_order_statuses_cached(
+        _session=session, utility_ids=utility_ids, translation=order_status_trans
+    )
+
+    with right_col:
+        create_order_button(
+            label=order_translation.create_order_button.label,
+            disabled=create_order_button_disabled,
+            session=session,
+            utility_id=selected_utility,
+            order_types=order_types,
+            order_statuses=order_statuses,
+            facilities=facilities,
+            checklists=checklists,
+            technicians=technicians,
+            translation=order_translation.create_order_form,
+            tz=tz,
+            user_id=user_id,
+            help_text=order_translation.create_order_button.help_text,
+        )
+
+    st.divider()
+    if not selected_utility:
+        st.info('Please select a utility above!', icon=ICON_INFO)
+        return
+
+    orders = get_all_orders_cached(
+        _session=session,
+        utility_ids=utility_ids,
+        order_type_trans=order_type_trans,
+        order_status_trans=order_status_trans,
+        tz=tz,
+    )
+    edit_orders_view(
         session=session,
+        orders=orders,
         order_types=order_types,
         order_statuses=order_statuses,
         facilities=facilities,
-        checklists=checklists,
         technicians=technicians,
-        translation=order_translation.create_order_form,
+        trans=edit_orders_view_trans,
+        edit_orders_df_trans=edit_orders_df_trans,
         tz=tz,
         user_id=user_id,
-        help_text=order_translation.create_order_button.help_text,
+        has_edit_permission=has_edit_permission,
     )
