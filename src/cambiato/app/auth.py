3d3b49dc0ea33879dc756dbd9827342ee174c33e
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2025-09-13 13:26:01 +0200

    Added permission handling to module app.auth
    The IntEnum `Permission` that defines the available permissions, which
    are then mapped to the minimum role that has this permission.
    
    Added `Permission.ORDERS_EDIT`, which is mapped to the `UserRole`
    meaning that any user with a role of `UserRole` or higher is allowed to
    edit orders. The mapping is done through the dictionary `permission_mapping`.
    
    The function `has_permission` is used to check if the current user
    has a certain permission.

--- src/cambiato/app/auth.py
+++ src/cambiato/app/auth.py
@@ -1,15 +1,89 @@
 r"""User authentication and authorization."""
 
+# Standard library
+from enum import IntEnum
+
 # Third party
 import streamlit as st
 import streamlit_passwordless as stp
+from streamlit_passwordless import AdminRole as AdminRole
+from streamlit_passwordless import SuperUserRole as SuperUserRole
+from streamlit_passwordless import UserRole as UserRole
+from streamlit_passwordless import ViewerRole as ViewerRole
 from streamlit_passwordless import authorized as authorized
 
 # Local
-from cambiato.app.session_state import AUTHENTICATED
+from cambiato import exceptions
+from cambiato.app.session_state import (
+    AUTHENTICATED,
+    USER_AUTHORIZED_PERMISSIONS,
+    USER_UNAUTHORIZED_PERMISSIONS,
+)
+from cambiato.models import User
 
 
-def authenticated(user: stp.User | None = None) -> bool:
+class Permission(IntEnum):
+    r"""Permissions for users to perform certain operations.
+
+    Members
+    -------
+    ORDERS_EDIT
+        A user is allowed to edit orders.
+    """
+
+    ORDERS_EDIT = 1
+
+
+permission_mapping = {Permission.ORDERS_EDIT: UserRole}
+
+
+def has_permission(user: User, permission: Permission) -> bool:
+    r"""Check if the `user` has specified `permission`.
+
+    Parameters
+    ----------
+    user : cambiato.models.User
+        The user for which to check for permissions.
+
+    permission : cambiato.app.auth.Permission
+        The permission to validate.
+
+    Returns
+    -------
+    bool
+        True if the user has specified `permission` and False otherwise.
+
+    Raises
+    ------
+    cambiato.CambiatoError
+        If `permission` is not mapped to a user role.
+    """
+
+    authorized_permissions = st.session_state.get(USER_AUTHORIZED_PERMISSIONS, set())
+    if permission in authorized_permissions:
+        return True
+
+    unauthorized_permissions = st.session_state.get(USER_UNAUTHORIZED_PERMISSIONS, set())
+    if permission in unauthorized_permissions:
+        return False
+
+    role = permission_mapping.get(permission)
+    if role is None:
+        raise exceptions.CambiatoError(f'Permission {permission!r} is not mapped to a role!')
+
+    has_permission = user.is_authorized(role=role)
+
+    if has_permission:
+        authorized_permissions.add(permission)
+        st.session_state[USER_AUTHORIZED_PERMISSIONS] = authorized_permissions
+    else:
+        unauthorized_permissions.add(permission)
+        st.session_state[USER_UNAUTHORIZED_PERMISSIONS] = unauthorized_permissions
+
+    return has_permission
+
+
+def authenticated(user: User | None = None) -> bool:
     r"""Check if the current user is authenticated.
 
     Parameters
