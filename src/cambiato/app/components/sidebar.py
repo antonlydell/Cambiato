5602826cdc288ea4a18a4e8d1f229b6e646f5a9d
Anton Lydell <37381421+antonlydell@users.noreply.github.com>
2025-09-13 14:31:36 +0200

    Updated the sidebar component to use the new sign_out_button
    Added support to supply the user accessing the application to avoid
    looking up the user in session state. Updated the calling code of the
    sidebar in the app router.

--- src/cambiato/app/components/sidebar.py
+++ src/cambiato/app/components/sidebar.py
@@ -2,24 +2,26 @@ r"""Sidebar components."""
 
 # Third party
 import streamlit as st
-import streamlit_passwordless as stp
 
 # Local
-from cambiato.app.session_state import AUTHENTICATED
+from cambiato.app.components.buttons import sign_out_button
+from cambiato.models import User
 
 
-def sidebar(authenticated: bool) -> None:
+def sidebar(is_authenticated: bool, user: User | None = None) -> None:
     r"""Render the sidebar.
 
     Parameters
     ----------
-    authenticated : bool
-        True if the user is authenticated and False otherwise.
+    is_authenticated : bool
+        True if the `user` is authenticated and False otherwise.
+
+    user : cambiato.models.User or None, default None
+        The user accessing the application. If None the user is not signed in yet.
     """
 
-    if not authenticated:
+    if not is_authenticated:
         return
 
     with st.sidebar:
-        if stp.sign_out_button():
-            st.session_state[AUTHENTICATED] = False
+        sign_out_button(user=user)
--- src/cambiato/app/main.py
+++ src/cambiato/app/main.py
@@ -8,8 +8,8 @@ import streamlit as st
 import streamlit_passwordless as stp
 
 # Local
-from cambiato.app import auth
 from cambiato.app._pages import Pages
+from cambiato.app.auth import get_current_user, is_authenticated
 from cambiato.app.components.sidebar import sidebar
 
 APP_PATH = Path(__file__)
@@ -19,16 +19,17 @@ def main() -> None:
     r"""The page router of the Cambiato web app."""
 
     stp.init_session_state()
-    is_authenticated = auth.is_authenticated()
+    user = get_current_user()
+    _is_authenticated = is_authenticated(user=user)
 
     pages = [
         st.Page(page=Pages.HOME, title='Home'),
         st.Page(page=Pages.SIGN_IN, title='Sign in and register', default=True),
         st.Page(page=Pages.ORDER, title='Order'),
     ]
-    page = st.navigation(pages, position='top' if is_authenticated else 'hidden')
+    page = st.navigation(pages, position='top' if _is_authenticated else 'hidden')
 
-    sidebar(authenticated=is_authenticated)
+    sidebar(is_authenticated=_is_authenticated, user=user)
 
     page.run()
 
